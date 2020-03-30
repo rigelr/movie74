3024a0f641656b2547c703fe678024c43edd91a4
rigelr <rigelrahmawati@gmail.com>
2020-03-30 16:40:45 +0700

    done intent on button pesan on movieactivity

--- app/src/main/java/com/rigelr/watchedmovie/Adapters/CardViewMovieAdapter.java
+++ app/src/main/java/com/rigelr/watchedmovie/Adapters/CardViewMovieAdapter.java
@@ -2,6 +2,7 @@ package com.rigelr.watchedmovie.Adapters;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.rigelr.watchedmovie.AboutActivity;
 import com.rigelr.watchedmovie.R;
 import com.rigelr.watchedmovie.models.Movie;
 
@@ -40,18 +42,25 @@ public class CardViewMovieAdapter extends RecyclerView.Adapter<CardViewMovieAdap
 
     @Override
     public void onBindViewHolder(@NonNull final CardViewViewHolder holder, int position) {
-        Movie movie = listMovie.get(position);
+        final Movie movie = listMovie.get(position);
         Glide.with(holder.itemView.getContext())
                 .load(movie.getPhoto())
                 .apply(new RequestOptions().override(350, 550))
                 .into(holder.imgPhoto);
         holder.tvName.setText(movie.getName());
         holder.tvDetail.setText(movie.getDetail());
-        holder.btnFavorite.setOnClickListener(new View.OnClickListener() {
+        holder.btnPesan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(holder.itemView.getContext(), "Favorite " +
+                Toast.makeText(holder.itemView.getContext(), "pesan " +
                         listMovie.get(holder.getAdapterPosition()).getName(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(holder.itemView.getContext(), AboutActivity.class);
+                Bundle b = new Bundle();
+                Intent message = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms",number,null));
+                message.putExtra("sms_body","Halo saya mau pesan film \t"+movie.getName()+"");
+
+                //begin activity
+                holder.itemView.getContext().startActivity(message);
             }
         });
         holder.btnShare.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +68,13 @@ public class CardViewMovieAdapter extends RecyclerView.Adapter<CardViewMovieAdap
             public void onClick(View v) {
                 Toast.makeText(holder.itemView.getContext(), "Share " +
                         listMovie.get(holder.getAdapterPosition()).getName(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(holder.itemView.getContext(), AboutActivity.class);
+                Bundle b = new Bundle();
+//                Intent message = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms",number,null));
+//                message.putExtra("sms_body","Halo saya mau pesan film \t"+movie.getName()+"");
+
+                //begin activity
+                holder.itemView.getContext().startActivity(intent);
 
             }
         });
@@ -72,13 +88,13 @@ public class CardViewMovieAdapter extends RecyclerView.Adapter<CardViewMovieAdap
     public class CardViewViewHolder extends RecyclerView.ViewHolder {
         ImageView imgPhoto;
         TextView tvName, tvDetail;
-        Button btnFavorite, btnShare;
+        Button btnPesan, btnShare;
         public CardViewViewHolder(@NonNull View itemView) {
             super(itemView);
             imgPhoto = itemView.findViewById(R.id.img_item_photo);
             tvName = itemView.findViewById(R.id.tv_item_name);
             tvDetail = itemView.findViewById(R.id.tv_item_detail);
-            btnFavorite = itemView.findViewById(R.id.btn_set_favorite);
+            btnPesan = itemView.findViewById(R.id.btn_set_pesan);
             btnShare = itemView.findViewById(R.id.btn_set_share);
         }
     }
