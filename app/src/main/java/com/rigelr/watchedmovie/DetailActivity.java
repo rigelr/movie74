82b59d15b5a1753c4ffe1868b4a3778df3ffabf9
rigelr <rigelrahmawati@gmail.com>
2020-04-03 09:41:23 +0700

    done onclick to detail activity show title and detail, photo not yet

--- app/src/main/java/com/rigelr/watchedmovie/DetailActivity.java
+++ app/src/main/java/com/rigelr/watchedmovie/DetailActivity.java
@@ -3,22 +3,38 @@ package com.rigelr.watchedmovie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.rigelr.watchedmovie.models.Movie;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_DATA_MOVIE = "EXTRA_DATA_MOVIE";
+    private TextView TVtitle;
+    private  TextView TVdesc;
+    private ImageView IVposter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         getSupportActionBar().setTitle("Detail Activity");
+        TVtitle = findViewById(R.id.txt_title);
+        TVdesc = findViewById(R.id.txt_desc);
+        IVposter = findViewById(R.id.imageView3);
 
         if (getIntent().hasExtra(EXTRA_DATA_MOVIE)) {
             Movie movie = getIntent().getParcelableExtra(EXTRA_DATA_MOVIE);
+//            Glide.with(itemView.getContext())
+//                    .load(movie.getPhoto())
+//                    .apply(new RequestOptions().override(350, 550))
+//                    .into(IVposter);
+            TVtitle.setText(movie.getName());
+            TVdesc.setText(movie.getDetail());
 
             Toast.makeText(this, movie.getName(), Toast.LENGTH_SHORT)
                     .show();
